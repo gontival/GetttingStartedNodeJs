22609cd785a3a6340da77b0bfc5b1c2e8fbb123b
MCNEXT\jlevant <jlevant@mcnext.com>
2017-02-17 16:50:43 +0100

    Creating a RESTful web service by using Node.js

---
+++ node_samples/math_service/app.js
@@ -0,0 +1,21 @@
+﻿    var express = require('express');
+    var app = express();
+    var formidable = require('formidable');
+    var math = require('math_example');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/addition', function (request, response) {
+    var x = Number(request.query.x);
+    var y = Number(request.query.y);
+    var result = math.addition(x, y);
+
+    response.writeHead('200', { 'Content-Type': 'application/json' });
+    response.end('{"result" : ' + result + '}');
+    console.log('Handled request for x= ' + x + 'y= ' + y);
+});
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port: ' + port);
+
